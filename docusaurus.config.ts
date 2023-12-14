4da42af848986f454a4ce658b997fcd381eb3631
Luca Giorgino <lucas.10m.lg@gmail.com>
2023-12-14 23:23:26 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lucagiorgino', // Usually your GitHub org/user name.
   projectName: 'lucagiorgino.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
